98c7078a517862daf46fb7159f7e460abd4a096a
Ben Kaiser <strmbrkr00@aol.com>
2017-01-25 13:06:44 -0700

    main

---
+++ assignment1/main.lua
@@ -0,0 +1,19 @@
+-- LuaIP image processing routines
+require "ip"   -- this loads the packed distributable
+local viz = require "visual"
+local il = require "il"
+local ourProcesses = require "pointprocess"
+
+imageMenu("Test", { {"Resize", ourProcesses.grayscale}})
+
+
+
+imageMenu("Help",
+  {
+    { "Help", viz.imageMessage( "Help", "To process an image:\n 1. File -> Open to select an image to process\n2. Click different menu options to perform computations\n" ..
+        "3. File-> Save if you desire to save to a file") },
+    { "About", viz.imageMessage( "CSC 442 Assignment 1", "Authors: Benjamin Kaiser and Taylor Doell\nClass: CSC442/542 Digital Image Processing\nDate: Spring 2017" ) },
+  }
+)
+
+start()
