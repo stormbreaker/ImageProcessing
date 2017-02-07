5df4f86a923326ccf4eee7af0422d153b85d9e14
doell4nuggets <doell4nuggets@gmail.com>
2017-02-06 20:01:02 -0700

    Fixed modified and automatic contrast stretch

--- assignment1/pointprocess.lua
+++ assignment1/pointprocess.lua
@@ -376,12 +376,17 @@ local function modifiedContrastStretch(img, darkPercent, lightPercent)
     max, min = min, max
   end
   
-  print("Max = " .. max)
-  print("Min = " .. min)
-  
   for row = 0, rows - 1 do
     for col = 0, columns - 1 do
-      img:at(row, col).y = math.floor((255 / (max - min)) * (img:at(row, col).y - min))
+      local pixVal = math.floor((255 / (max - min)) * (img:at(row, col).y - min))
+      
+      if pixVal > 255 then
+        pixVal = 255
+      elseif pixVal < 0 then
+        pixVal = 0
+      end
+      
+      img:at(row, col).y = pixVal
     end
   end
   
@@ -393,7 +398,7 @@ local function benAutoContrastStretch(img)
 end
 
 local function automaticContrastStretch(img)
-  return modifiedContrastStretch(img, 0, 0)
+  return modifiedContrastStretch(img, 5, 5)
 end
 
 local function histogramDisplay(img)
