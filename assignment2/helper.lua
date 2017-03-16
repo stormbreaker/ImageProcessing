68eb8e635803791c9103ef5349e541ddc2704bdb
Ben Kaiser <strmbrkr00@aol.com>
2017-03-16 11:26:15 -0600

    helper.lua commented

--- assignment2/helper.lua
+++ assignment2/helper.lua
@@ -1,3 +1,8 @@
+--[[
+  Authors: Benjamin Kaiser and Taylor Doell
+  This file contains helper functions that may be called by our other functions.  These functions include reflection of image borders and rotation of filters.  
+]]
+
 require "ip"
 
 local il = require "il"
@@ -7,21 +12,29 @@ local math = require "math"
 local tempFilter = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}
 
 --[[
-Author: Benjamin Kaiser
-Description: 
+  Author: Benjamin Kaiser
+  Description: This function takes in an x and y coordinates as well as
+  the max x and y coordinates to perform reflection of image borders.
+  If the item trying to be processed is much larger than the size of
+  the image, it bounces back and forth until it settles on a pixel which
+  is actually part of the image.  
 ]]
 local function reflection(x, y, maxX, maxY)
   
+  -- Loop until we found a pixel inside of the image
   while ((x < 0 or x > maxX) or (y < 0 or y > maxY)) do
-    
+    -- pixel falls out on the left side
     if x < 0 then
       x = 0 - x - 1
+    -- pixel falls out on the right side
     elseif x > maxX then
       x = 2 * maxX - x + 1
     end
     
+    --pixel falls out above
     if y < 0 then
       y = 0 - y - 1
+    -- pixel falls out below
     elseif y > maxY then
       y = 2 * maxY - y + 1
     end
